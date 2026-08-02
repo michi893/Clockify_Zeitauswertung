3fc7092447ac2f80db54af889bd260ccd60fee84
michi893 <frei.michi@gmx.net>
2026-08-02 22:01:02 +0200

    improved so dass immer eine antwort gegeben wird.

--- getWorkspaceID.py
+++ getWorkspaceID.py
@@ -1,19 +1,24 @@
 import requests
+import streamlit as st
 
 url = "https://api.clockify.me/api/v1/user"
 
 
-def get_workspace_id(API_KEY, headers):
-    # Get user information
+def get_workspace_id(api_key, headers):
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        user = response.json()
-        # USER_ID = user["id"]
-        WORKSPACE_ID = user["activeWorkspace"]
-        username = user["name"]
-        # print(f"Name: {user['name']}")
-        # print(f"Email: {user['email']}")
-        # print(f"Active Workspace: {user['activeWorkspace']}")
-        # print(f"Default Workspace: {user['defaultWorkspace']}")
-    return WORKSPACE_ID, username
+    if response.status_code != 200:
+        st.error(f"Clockify API-Fehler ({response.status_code}): {response.text}")
+        st.stop()
+
+    user = response.json()
+
+    workspace_id = user["activeWorkspace"]
+    username = user["name"]
+    # USER_ID = user["id"]
+    # print(f"Name: {user['name']}")
+    # print(f"Email: {user['email']}")
+    # print(f"Active Workspace: {user['activeWorkspace']}")
+    # print(f"Default Workspace: {user['defaultWorkspace']}")
+
+    return workspace_id, username
